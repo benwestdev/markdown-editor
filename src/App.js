65216c809fd8d873e09b3704e5f3b2e506fcabb3
Ben West <benwestdev@gmail.com>
2020-09-29 15:11:19 -0400

    add react-markdown and use to show markdown

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+export default function App() {
+  const [markdown, setMarkdown] = useState("# sup");
+
+  return (
+    <div className="app">
+      <textarea
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+      />
+      <ReactMarkdown source={markdown} />
+    </div>
+  );
 }
-
-export default App;
